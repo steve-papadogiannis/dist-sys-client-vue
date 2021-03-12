560b811cabb3b9e356e5e1e62481fe350fd686a7
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-03-12 09:39:18 +0200

    Adding axios dep, adding getDirections action and adding polyline rendering functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    results: []
+  },
+  mutations: {
+    storeResult(state, results) {
+      state.results = results;
+    }
+  },
+  actions: {
+    getDirections({ commit }, jsonBody) {
+      axios
+        .post("http://localhost:8383/getDirections", jsonBody)
+        .then(response => {
+          console.log(response);
+          commit("storeResult", response.data);
+        })
+        .catch(error => {
+          console.error("There was an error!", error);
+        });
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,36 @@
 <template>
-  <GmapMap
-    id="googleMap"
-    :center="{ lat: 38.05629563288619, lng: 23.80496830013357 }"
-    :zoom="15"
-    @rightclick="rightClick"
-    map-type-id="terrain"
-    style="width: 100%; height: 500px"
-  >
-    <GmapMarker
-      v-for="(marker, index) in markers"
-      :key="index"
-      :position="marker.position"
-      @rightclick="markerRightClick(index)"
-    />
-  </GmapMap>
+  <div id="root">
+    <GmapMap
+      id="googleMap"
+      :center="{ lat: 38.05629563288619, lng: 23.80496830013357 }"
+      :zoom="15"
+      @rightclick="mapRightClick"
+      map-type-id="terrain"
+      style="width: 100%; height: 500px"
+    >
+      <GmapMarker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker.position"
+        @rightclick="markerRightClick(index)"
+      />
+
+      <GmapPolyline :path="this.results" />
+    </GmapMap>
+    <button
+      type="submit"
+      value="Get Directions"
+      style="width: 200px; height:60px"
+      @click="submit"
+    >
+      <span>Get Directions</span>
+    </button>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex/dist/vuex.mjs";
+
 export default {
   name: "Home",
   data() {
@@ -24,8 +38,14 @@ export default {
       markers: []
     };
   },
+  computed: {
+    ...mapState(["results"])
+  },
   methods: {
-    rightClick(event) {
+    mapRightClick(event) {
+      if (this.markers.length === 2) {
+        this.markers.splice(0, 1);
+      }
       this.markers.push({
         position: {
           lat: event.latLng.lat(),
@@ -35,7 +55,24 @@ export default {
     },
     markerRightClick(index) {
       this.markers.splice(index, 1);
-    }
+    },
+    submit() {
+      if (this.markers.length === 2) {
+        const startGeoPoint = this.markers[0];
+        const endGeoPoint = this.markers[1];
+        this.getDirections({
+          startGeoPoint: {
+            latitude: startGeoPoint.position.lat,
+            longitude: startGeoPoint.position.lng
+          },
+          endGeoPoint: {
+            latitude: endGeoPoint.position.lat,
+            longitude: endGeoPoint.position.lng
+          }
+        });
+      }
+    },
+    ...mapActions(["getDirections"])
   }
 };
 </script>
