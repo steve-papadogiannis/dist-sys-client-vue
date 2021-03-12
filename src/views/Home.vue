0c894a57cf5328c707d0bd13b0b2cc852d04d9f0
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-03-12 08:28:44 +0200

    Adding marker adding functionality.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,41 @@
 <template>
   <GmapMap
+    id="googleMap"
     :center="{ lat: 38.05629563288619, lng: 23.80496830013357 }"
     :zoom="15"
+    @rightclick="rightClick"
     map-type-id="terrain"
     style="width: 100%; height: 500px"
-  />
+  >
+    <GmapMarker
+      v-for="(marker, index) in markers"
+      :key="index"
+      :position="marker.position"
+      @rightclick="markerRightClick(index)"
+    />
+  </GmapMap>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      markers: []
+    };
+  },
+  methods: {
+    rightClick(event) {
+      this.markers.push({
+        position: {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+        }
+      });
+    },
+    markerRightClick(index) {
+      this.markers.splice(index, 1);
+    }
+  }
 };
 </script>
